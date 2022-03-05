e161ef07effcc81fb4f56a2c5ab649a33d805454
Electropompa <admin@zilmet.ru>
2022-03-05 14:32:03 +0300

    init

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let concatArray = [];
+
+  if (Array.isArray(matrix)) {
+    if ( matrix.length != 0 ) {
+
+      for ( let i = 0; i < matrix.length; i++ ) {
+        concatArray = ( i % 2 === 0) ? concatArray.concat(matrix[i]) : concatArray.concat(matrix[i].reverse());
+      }
+
+    }
+  }
+
+  return concatArray;
 }
